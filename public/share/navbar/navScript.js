2f4ae75240234473ee2b6afd67e09dc8e0686b19
danny8508021 <dannywang0802@yahoo.com.tw>
2017-11-16 22:13:30 +0800

    Rebuild structure

---
+++ public/share/navbar/navScript.js
@@ -0,0 +1,18 @@
+document.getElementById("navMenu").innerHTML =
+  '<div class="container-fluid">'+
+    '<div class="navbar-header">'+
+      '<a class="navbar-brand" href="/static/homepage">Lin-Shan Lee</a>'+
+    '</div>'+
+    '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'+
+      '<ul class="nav navbar-nav">'+
+        '<li><a href="/static/about"><span id="chinese">關於</span><br>ABOUT</a></li>'+
+        '<li><a href="/static/personal"><span id="chinese">個人</span><br>PERSONAL</a></li>'+
+        '<li><a href="/static/education"><span id="chinese>教學</span><br>EDUCATION</a></li>'+
+        '<li><a href="/static/research"><span id="chinese">研究</span><br>RESEARCH</a></li>'+
+        '<li><a href="/static/admin"><span id="chinese">行政事務</span><br>ADMINISTRATION</a></li>'+
+        '<li><a href="/static/internation"><span id="chinese">國際事務</span><br>INTERNATIONAL</a></li>'+
+        '<li><a href="/static/talks"><span id="chinese">演講</span></span><br>TALKS</a></li>'+
+        '<li><a href="/static/honor"><span id="chinese">榮譽</span><br>HONORS</a></li>'+
+      '</ul>'+
+    '</div>'+
+  '</div>';
--- server.js
+++ server.js
@@ -3,14 +3,10 @@ const app  = express();
 const http = require('http').Server(app);
 const path = require('path')
 
-app.use('/static/html', express.static(path.join(__dirname, 'public/html')));
-app.use('/static/css', express.static(path.join(__dirname, 'public/css')));
-app.use('/static/img', express.static(path.join(__dirname, 'public/img')));
-app.use('/static/js', express.static(path.join(__dirname, 'public/js')));
-
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/html/index.html'));
+  res.sendFile(path.join(__dirname, 'public/homepage/index.html'));
 });
 
 http.listen(3000, function(){
