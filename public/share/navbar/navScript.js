6f1d6f35a14ee07289795fc818da579381f4cb6a
awinder0230 <awinder0230@gmail.com>
2017-11-16 22:47:02 +0800

    homepage

--- public/share/navbar/navScript.js
+++ public/share/navbar/navScript.js
@@ -5,14 +5,14 @@ document.getElementById("navMenu").innerHTML =
     '</div>'+
     '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'+
       '<ul class="nav navbar-nav">'+
-        '<li><a href="/static/about"><span id="chinese">關於</span><br>ABOUT</a></li>'+
-        '<li><a href="/static/personal"><span id="chinese">個人</span><br>PERSONAL</a></li>'+
-        '<li><a href="/static/education"><span id="chinese>教學</span><br>EDUCATION</a></li>'+
-        '<li><a href="/static/research"><span id="chinese">研究</span><br>RESEARCH</a></li>'+
-        '<li><a href="/static/admin"><span id="chinese">行政事務</span><br>ADMINISTRATION</a></li>'+
-        '<li><a href="/static/internation"><span id="chinese">國際事務</span><br>INTERNATIONAL</a></li>'+
-        '<li><a href="/static/talks"><span id="chinese">演講</span></span><br>TALKS</a></li>'+
-        '<li><a href="/static/honor"><span id="chinese">榮譽</span><br>HONORS</a></li>'+
+        '<li><a href="/static/about"><span id="chinese">關於</span><br>About</a></li>'+
+        '<li><a href="/static/personal"><span id="chinese">個人</span><br>Personal</a></li>'+
+        '<li><a href="/static/education"><span id="chinese">教學</span><br>Education</a></li>'+
+        '<li><a href="/static/research"><span id="chinese">研究</span><br>Research</a></li>'+
+        '<li><a href="/static/admin"><span id="chinese">行政事務</span><br>Administration</a></li>'+
+        '<li><a href="/static/internation"><span id="chinese">國際事務</span><br>Internation</a></li>'+
+        '<li><a href="/static/talks"><span id="chinese">演講</span></span><br>Talks</a></li>'+
+        '<li><a href="/static/honor"><span id="chinese">榮譽</span><br>Honor</a></li>'+
       '</ul>'+
     '</div>'+
   '</div>';
