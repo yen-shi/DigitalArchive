90f7010e7f3a8eebbb0316adc71bb009a9ba6b8a
awinder0230 <awinder0230@gmail.com>
2017-11-30 21:40:02 +0800

    navbar

--- public/share/navbar/navScript.js
+++ public/share/navbar/navScript.js
@@ -1,20 +1,23 @@
 document.getElementById("navMenu").innerHTML =
   '<div class="container-fluid">'+
-    '<div class="row">'+
-      '<div class="navbar-header col-lg-2 col-md-2 col-sm-2">'+
-        '<a class="navbar-brand" href="/static/homepage">Lin-Shan Lee</a>'+
-      '</div>'+
-      '<div class="collapse navbar-collapse col-lg-10 col-md-10 col-sm-2 " id="bs-example-navbar-collapse-1">'+
-        '<ul class="nav navbar-nav">'+
-          '<li><a href="/static/about"><span id="chinese">關於</span><br>About</a></li>'+
-          '<li><a href="/static/education"><span id="chinese">教學篇</span><br>Education</a></li>'+
-          '<li><a href="/static/research"><span id="chinese">研究篇</span><br>Research</a></li>'+
-          '<li><a href="/static/admin"><span id="chinese">行政事務篇</span><br>Administration</a></li>'+
-          '<li><a href="/static/internation"><span id="chinese">國際事務篇</span><br>Internation</a></li>'+
-          '<li><a href="/static/honor"><span id="chinese">榮譽篇</span><br>Honor</a></li>'+
-          '<li><a href="/static/talks"><span id="chinese">演說篇</span></span><br>Talks</a></li>'+
-         '<li><a href="/static/personal"><span id="chinese">個人篇</span><br>Personal</a></li>'+
-        '</ul>'+
-      '</div>'+
+    '<div class="navbar-header">'+
+      '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">'+
+        '<span class="icon-bar"></span>'+
+        '<span class="icon-bar"></span>'+
+        '<span class="icon-bar"></span>'+                     
+      '</button>'+
+      '<a class="navbar-brand" href="/static/homepage">Lin-Shan Lee</a>'+
+    '</div>'+
+    '<div class="collapse navbar-collapse" id="myNavbar">'+
+      '<ul class="nav navbar-nav">'+
+        '<li><a href="/static/about"><span id="navbar-chinese">關於</span><br>About</a></li>'+
+        '<li><a href="/static/education"><span id="navbar-chinese">教學篇</span><br>Education</a></li>'+
+        '<li><a href="/static/research"><span id="navbar-chinese">研究篇</span><br>Research</a></li>'+
+        '<li><a href="/static/admin"><span id="navbar-chinese">行政事務篇</span><br>Administration</a></li>'+
+        '<li><a href="/static/internation"><span id="navbar-chinese">國際事務篇</span><br>Internation</a></li>'+
+        '<li><a href="/static/honor"><span id="navbar-chinese">榮譽篇</span><br>Honor</a></li>'+
+        '<li><a href="/static/talks"><span id="navbar-chinese">演說篇</span></span><br>Talks</a></li>'+
+        '<li><a href="/static/personal"><span id="navbar-chinese">個人篇</span><br>Personal</a></li>'+
+      '</ul>'+
     '</div>'+
   '</div>';
