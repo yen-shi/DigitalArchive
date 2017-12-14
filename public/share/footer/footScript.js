686637a39636383907f3c82037fff4bd2cc198a7
awinder0230 <awinder0230@gmail.com>
2017-12-14 23:33:13 +0800

    footer

---
+++ public/share/footer/footScript.js
@@ -0,0 +1,15 @@
+document.getElementById("footerMenu").innerHTML =
+  '<div class="container-fluid">'+
+    '<div class="row">'+
+      '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">'+
+        '<div class="contact">'+
+          'Email: <a href="mailto:lslee@cc.ee.ntu.edu.tw">lslee@cc.ee.ntu.edu.tw</a> | Phone: +886 (2) 2363-5251 ext. 521'+
+		'</div>'+
+      '</div>'+
+      '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">'+
+        '<div class="address">'+
+          'Work Address: EE2-520, Department of Electrical Engineering, National Taiwan University, Taipei 106, Taiwan'
+        '</div>'+
+      '</div>'+
+    '</div>'+
+  '</div>';
