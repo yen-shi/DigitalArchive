1336f10054cedf205f84fb602bbe53b30a67d684
danny8508021 <dannywang0802@yahoo.com.tw>
2018-01-19 22:00:37 +0800

    Complete footer

--- public/share/footer/footScript.js
+++ public/share/footer/footScript.js
@@ -1,15 +1,27 @@
+function contactComponent(iconName, content) {
+  component =
+    '<div class="col-xs-4">' +
+      '<div class="footer-contact">' +
+        '<div class="footer-icon">' +
+          '<span class="glyphicon ' + iconName + '" aria-hidden="true"></span>' +
+        '</div>' +
+        '<div class="footer-text">' +
+          content +
+        '</div>' +
+      '</div>' +
+    '</div>';
+  return component;
+}
+
 document.getElementById("footerMenu").innerHTML =
-  '<div class="container-fluid">'+
-    '<div class="row">'+
-      '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">'+
-        '<div class="contact">'+
-          'Email: <a href="mailto:lslee@cc.ee.ntu.edu.tw">lslee@cc.ee.ntu.edu.tw</a> | Phone: +886 (2) 2363-5251 ext. 521'+
-		'</div>'+
-      '</div>'+
-      '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">'+
-        '<div class="address">'+
-          'Work Address: EE2-520, Department of Electrical Engineering, National Taiwan University, Taipei 106, Taiwan'
-        '</div>'+
-      '</div>'+
+  '<div class="container-fluid">' +
+    '<div class="row">' +
+      contactComponent('glyphicon-earphone',
+                       '<span style="font-size:18px">+886 (2) 2363-5251 ext. 521</span>') +
+      contactComponent('glyphicon-envelope',
+                       '<span style="font-size:18px"><a id="footer-email" href="mailto:lslee@cc.ee.ntu.edu.tw">lslee@cc.ee.ntu.edu.tw</a></span>') +
+      contactComponent('glyphicon-home', 
+                       '<span>台北市大安區羅斯福路四段 1 號</span>' + '<br>' +
+                       '<span style="font-size:12px">No. 1, Sec. 4, Roosevelt Rd., Taipei 10617 Taiwan (R.O.C.)</span>') +
     '</div>'+
   '</div>';
