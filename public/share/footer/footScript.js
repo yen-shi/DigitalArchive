912db9a7bcc0a2f49390cb6bbb8b5046970bd679
danny8508021 <dannywang0802@yahoo.com.tw>
2018-01-25 16:26:00 +0800

    Change font family

--- public/share/footer/footScript.js
+++ public/share/footer/footScript.js
@@ -17,9 +17,9 @@ document.getElementById("footerMenu").innerHTML =
   '<div class="container-fluid">' +
     '<div class="row">' +
       contactComponent('glyphicon-earphone',
-                       '<span style="font-size:18px">+886 (2) 2363-5251 ext. 521</span>') +
+                       '<span style="font-size:16px">+886 (2) 2363-5251 ext. 521</span>') +
       contactComponent('glyphicon-envelope',
-                       '<span style="font-size:18px"><a id="footer-email" href="mailto:lslee@cc.ee.ntu.edu.tw">lslee@cc.ee.ntu.edu.tw</a></span>') +
+                       '<span style="font-size:16px"><a id="footer-email" href="mailto:lslee@cc.ee.ntu.edu.tw">lslee@cc.ee.ntu.edu.tw</a></span>') +
       contactComponent('glyphicon-home', 
                        '<span>台北市大安區羅斯福路四段 1 號</span>' + '<br>' +
                        '<span style="font-size:12px">No. 1, Sec. 4, Roosevelt Rd., Taipei 10617 Taiwan (R.O.C.)</span>') +
