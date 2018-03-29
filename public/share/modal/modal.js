7c9f71ce75abeaf8e2943a7a207a5ebb11a6fae4
yen-shi <dannywang0802@yahoo.com.tw>
2018-03-29 11:36:28 +0800

    Add talks modal

---
+++ public/share/modal/modal.js
@@ -0,0 +1,13 @@
+// <!-- Modal -->
+// <!-- Ref: https://coolestguidesontheplanet.com/bootstrap/modal.php -->
+document.getElementById('biographyModal').innerHTML =
+ `<div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <button type="button" class="close modal-close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      <div class="modal-body">
+        ...
+      </div>
+    </div>
+  </div>`;
