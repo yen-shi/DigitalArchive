5770f933213ab213104a94735c4e350278baf0cb
awinder0230 <awinder0230@gmail.com>
2018-08-03 01:08:40 +0800

    honor title update

--- public/honor/js/honor.js
+++ public/honor/js/honor.js
@@ -63,13 +63,14 @@ function choose(type) {
 }
 
 modalContents = {
-  '總統科學獎': `<iframe class="modal-movie" src="https://www.youtube.com/embed/NulBlWIhfaM?rel=0&amp;start=565" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  '總統科學獎': `<h4 class="modal-text-title">電資學院熱烈慶賀李琳山教授榮獲總統科學獎</h4>
+  <iframe class="modal-movie" src="https://www.youtube.com/embed/NulBlWIhfaM?rel=0&amp;start=565" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
   <p class="modal-caption">Video 1 - [台灣名人堂] 2015 總統科學獎特輯</p><p class="modal-text">李琳山教授在1979年中美斷交臺灣風雨飄搖之際放棄在美安定優裕之研究環境回國,在當時相當艱困的國內學術條件下打造在臺灣的研究基地,長期鑽研語音訊號之電腦處理三十餘年並開花結果。他的研究在國際學術界被高度肯定,並在諸多國際學術事務中擔任積極角色並有突出貢獻,有效提升臺灣在國際學術界之地位。他在臺大電機系及資訊系任教三十餘年,曾任臺大資訊系主任5年、中研院資訊所所長6年、臺大研發長3年、臺大電機資訊學院院長3年,任內對相關領域之學術教育及典章制度著有貢獻,培育學生無數,遍佈今日國內外學術界、產業界,包括不少領袖人物。李教授在研究上的傑出成就及貢獻略述如下:1. 早期對華語語音及語言處理之開拓性貢獻李教授為華語語音處理極少數拓荒者之一。他針對華語文之特有結構,探討在這些特有結構下更有效處理華語之方法;逐年發展出當時最領先之華語各項技術,建構出基礎技術體系。他針對華語所有單音作了最早最完整的電腦處理分析,也針對華語文句建立了最早最完整的電腦分析文法及剖析器,可分析非常複雜的文句;他首先建立系統化的華語韻律規則並用以合成語音訊號,今日韻律仍是華語語音合</p>
   <img class="modal-images-h" src="/Digital_Archive/public/honor/img/2015_President_1.jpg"></img>
   <p class="modal-caption">Figure 1 - 總統科學獎獎杯</p>
   <img class="modal-images-w" src="/Digital_Archive/public/honor/img/2015_President_2.jpg"></img>
   <p class="modal-caption">Figure 2 - 總統科學獎獎狀</p>`,
-  '中央研究院院士': `<h4 class="modal-text-title">電資學院熱烈慶賀李琳山教授榮任中研院工程組第一屆唯一國內院士</h4>
+  '中央研究院院士': `<h4 class="modal-text-title">慶賀李琳山教授榮任中研院工程組第一屆唯一國內院士</h4>
   <img class="modal-images-w" src="/Digital_Archive/public/honor/img/2016_Academic_Sinica_1.png"></img>
   <p class="modal-caption">Figure 1 - 台大網站首頁校訊</p>
   <p class="modal-text">在七月初的中研院院士會議中，選出了工程科學組第一屆共6位院士。本校電資學院李琳山教授為6人之中唯一長期在國內耕耘獲致肯定者，其他5位當選人均為海外學者。李教授亦為今日本校工程領域專任教授中唯一榮任中研院院士者。電資學院師生聞訊莫不奔走相告額手稱慶，乃在8月1日舉辦慶賀活動。楊校長親臨致賀，並表示電資學院強將如雲，期待未來有更多人獲國內外各項肯定。李嗣涔前校長亦親臨致賀，並提到李琳山教授在研發會主委任內所完成的臺大第一期邁頂計畫書為臺大邁向頂尖大學描繪出最早的基礎藍圖。李琳山教授希望把所有成果及榮譽歸給過去三十七年來電資學院共兩百多位碩博士生，以及教這些學生各種學問使他們能作最好的研究的全體老師，「這項榮譽是大家的，謝謝大家」。</p>
