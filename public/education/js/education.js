1f94f1145804eb36783b3a0565f533e62e5b3c25
yen-shi <dannywang0802@yahoo.com.tw>
2018-09-23 23:43:07 +0800

    Complete education pages 1 & 2 template

---
+++ public/education/js/education.js
@@ -0,0 +1,38 @@
+const albums = {
+  '台大電機系': [ '/DigitalArchive/public/education/img/album-1.png',
+                  '/DigitalArchive/public/education/img/album-2.png',
+                  '/DigitalArchive/public/education/img/album-2.png',
+                  '/DigitalArchive/public/education/img/album-1.png', ]
+};
+
+function getHeader(title) {
+  return `<div class="col-xs-12 albums-header">
+      <div class="header-wrapper">
+        <div class="upper-line"></div>
+        <h1 class="header-text">${title}</h1>
+      </div>
+    </div>`;
+}
+
+function getAlbumsString(title) {
+  let ret = `<div class="container album-wrapper">`;
+  for(let i = 0; i < albums[title].length; i += 1) {
+    ret += `<div class="album col-sm-6 col-xs-12">
+        <img class="album-img-center-cropped"
+             src="${albums[title][i]}">
+        </img>
+      </div>`;
+  }
+  ret += '</div>';
+  return ret;
+}
+
+function getAlbum(name) {
+  mainSection = document.getElementsByClassName('main-section')[0];
+  mainSection.style.display = 'none';
+  albumSection = document.getElementsByClassName('album-section')[0];
+  let content = '';
+  content += getHeader(name);
+  content += getAlbumsString(name);
+  albumSection.innerHTML = content;
+}
