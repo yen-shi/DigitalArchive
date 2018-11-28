8eb55d6a7fe7affba62e5c5554c4987909022449
yen-shi <dannywang0802@yahoo.com.tw>
2018-11-28 17:37:35 +0800

    Add album and photos to education

--- public/education/js/education.js
+++ public/education/js/education.js
@@ -1,17 +1,32 @@
+// 相簿名稱 + 封面
 const albums = {
-  '台大電機系': [ '/DigitalArchive/public/education/img/album-1.png',
-                  '/DigitalArchive/public/education/img/album-2.png',
-                  '/DigitalArchive/public/education/img/album-2.png',
-                  '/DigitalArchive/public/education/img/album-1.png', ],
-  '台大資工系': [ '/DigitalArchive/public/education/img/album-1.png',
-                  '/DigitalArchive/public/education/img/album-2.png',
-                  '/DigitalArchive/public/education/img/album-2.png',
-                  '/DigitalArchive/public/education/img/album-1.png', ],
-  '語音處理實驗室': [ '/DigitalArchive/public/education/img/album-1.png',
-                  '/DigitalArchive/public/education/img/album-2.png',
-                  '/DigitalArchive/public/education/img/album-2.png',
-                  '/DigitalArchive/public/education/img/album-1.png', ]
+  '台大電機系': [ ['2017應屆畢業生團拍', '/DigitalArchive/public/education/img/album-1.png'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/album-2.png'],
+                  ['2017應屆畢業生團拍', '/DigitalArchive/public/education/img/album-2.png'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/album-1.png'], ],
+  '台大資工系': [ ['2017應屆畢業生團拍', '/DigitalArchive/public/education/img/album-1.png'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/album-2.png'],
+                  ['2017應屆畢業生團拍', '/DigitalArchive/public/education/img/album-2.png'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/album-1.png'], ],
+  '語音處理實驗室': [ ['2017應屆畢業生團拍', '/DigitalArchive/public/education/img/album-1.png'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/album-2.png'],
+                  ['2017應屆畢業生團拍', '/DigitalArchive/public/education/img/album-2.png'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/album-1.png'], ],
 };
+
+// 照片名稱 + 照片
+const album = {
+  '2017應屆畢業生團拍': [ ['2017應屆畢業生團拍', '/DigitalArchive/public/education/img/團拍-1.jpg'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/團拍-2.jpg'], ],
+  '2017電機系畢業典禮': [ ['2017應屆畢業生團拍', '/DigitalArchive/public/education/img/畢典-1.jpg'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/畢典-2.jpg'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/畢典-3.jpg'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/畢典-4.jpg'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/畢典-5.jpg'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/畢典-6.jpg'],
+                  ['2017電機系畢業典禮', '/DigitalArchive/public/education/img/畢典-7.jpg'], ],
+}
+
 
 function getHeader(title) {
   return `<div class="col-xs-12 albums-header">
@@ -25,10 +40,39 @@ function getHeader(title) {
 function getAlbumsString(title) {
   let ret = `<div class="container album-wrapper">`;
   for(let i = 0; i < albums[title].length; i += 1) {
-    ret += `<div class="album col-sm-6 col-xs-12">
+    ret += `<div class="albums col-sm-6 col-xs-12">
         <img class="album-img-center-cropped"
-             src="${albums[title][i]}">
+             onclick="getAlbum('${albums[title][i][0]}')"
+             src="${albums[title][i][1]}">
         </img>
+      </div>`;
+  }
+  ret += '</div>';
+  return ret;
+}
+
+function getAlbums(name) {
+  mainSection = document.getElementsByClassName('main-section')[0];
+  mainSection.style.display = 'none';
+  albumSection = document.getElementsByClassName('album-section')[0];
+  let content = '';
+  content += getHeader(name);
+  content += getAlbumsString(name);
+  albumSection.innerHTML = content;
+  albumSection.style.display = "block"
+}
+
+function getAlbumString(title) {
+  let ret = `<div class="container album-wrapper">`;
+  for(let i = 0; i < album[title].length; i += 1) {
+    ret += `<div class="album col-sm-3 col-xs-6">
+        <div class="box-50-50">
+          <div class="content-50-50">
+            <img class="album-img-center-cropped photo"
+                 src="${album[title][i][1]}">
+            </img>
+          </div>
+        </div>
       </div>`;
   }
   ret += '</div>';
@@ -41,7 +85,7 @@ function getAlbum(name) {
   albumSection = document.getElementsByClassName('album-section')[0];
   let content = '';
   content += getHeader(name);
-  content += getAlbumsString(name);
+  content += getAlbumString(name);
   albumSection.innerHTML = content;
   albumSection.style.display = "block"
 }
